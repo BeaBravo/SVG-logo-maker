6ad0351e68bbc2432906a80b27a9101153393b4a
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-19 22:13:24 -0700

    created function getData to writefile and generate logo

--- index.js
+++ index.js
@@ -2,22 +2,17 @@
 const LogoInput = require("./lib/user-input");
 const { createSVG } = require("./lib/document");
 const fs = require("fs");
-const { join } = require("path");
 
 const logoInput = new LogoInput();
-
-// logoInput.getAcronym();
 
 async function getData() {
   const acron = await logoInput.getAcronym();
   const textColor = await logoInput.getTextColor();
   const shape = await logoInput.getShape();
   const shapeColor = await logoInput.getshapeColor();
-  console.log(
-    `your acronym is ${acron} in text color ${textColor} shape: ${shape} filled in ${shapeColor}`
-  );
-  const innerHTML = await createSVG(acron, textColor, shape, shapeColor);
-  console.log(innerHTML);
+  const innerHTML = createSVG(acron, textColor, shape, shapeColor);
+  fs.writeFileSync(`./examples/${acron}.svg`, innerHTML);
+  console.log(`Generated ${acron}.svg -> check it out in the examples folder`);
 }
 
 getData();
