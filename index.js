e4ce8dd2dabd557161392549f6e05e3a7ee12513
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-19 22:00:52 -0700

    create async function getData to get data from inquirer

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 //Dependencies
 const LogoInput = require("./lib/user-input");
+const { createSVG } = require("./lib/document");
+const fs = require("fs");
+const { join } = require("path");
 
 const logoInput = new LogoInput();
 
-logoInput.getAcronym();
+// logoInput.getAcronym();
+
+async function getData() {
+  const acron = await logoInput.getAcronym();
+  const textColor = await logoInput.getTextColor();
+  const shape = await logoInput.getShape();
+  const shapeColor = await logoInput.getshapeColor();
+  console.log(
+    `your acronym is ${acron} in text color ${textColor} shape: ${shape} filled in ${shapeColor}`
+  );
+  const innerHTML = await createSVG(acron, textColor, shape, shapeColor);
+  console.log(innerHTML);
+}
+
+getData();
