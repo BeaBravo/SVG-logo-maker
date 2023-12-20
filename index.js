f3ff94c014a7cdf0771a022af2f9dea5ef0a2937
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-19 17:33:04 -0700

    finished inquirer

--- index.js
+++ index.js
@@ -4,36 +4,3 @@ const LogoInput = require("./lib/user-input");
 const logoInput = new LogoInput();
 
 logoInput.getAcronym();
-
-//user input
-// inquirer
-//   .prompt([
-//     {
-//       type: "input",
-//       message: "Enter your three character acronym: ",
-//       name: "acronym",
-//     },
-//     {
-//       type: "input",
-//       message: "Text color, enter colour keyword or hexadecima code: ",
-//       name: "textColor",
-//     },
-//     {
-//       type: "list",
-//       message: "Choose your logo shape: ",
-//       name: "shape",
-//       choices: ["triangle", "circle", "square"],
-//     },
-//     {
-//       type: "input",
-//       message: "Shape fill color, enter colour keyword or hexadecimal code: ",
-//       name: "shapeColor",
-//     },
-//   ])
-//   .then(({ acronym, textColor, shape, shapeColor }) => {
-//     console.log(acronym, textColor, shape, shapeColor);
-//     if (acronym.length > 3) {
-//       console.log("Acronym must be max 3 characters long, try again: ");
-//       return;
-//     }
-//   });
